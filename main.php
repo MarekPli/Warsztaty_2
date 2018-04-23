11a29a2f7bb37dae70480d5da67de07037280032
MarekPli <marekpl24@wp.pl>
2018-04-23 07:29:33 +0200

    class Use: all tests

--- main.php
+++ main.php
@@ -1,11 +1,13 @@
 <?php
 include_once (__DIR__ . '/scr/User.php');
 include_once (__DIR__ . '/scr/DB_open.php');
-require('vendor/autoload.php');
+include_once (__DIR__ . '/scr/form_asking.html');
+require_once('vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 $faker = \Faker\Factory::create('pl');
 
-function addRandomUser (PDO $base) {
+function createRandomUser (PDO $base) {
     global $faker;
 
     $newUser = new User();
@@ -15,9 +17,82 @@ function addRandomUser (PDO $base) {
     $newUser->setEmail($faker->email);
     $newUser->setPassword($faker->domainWord);
 
-    $newUser->saveToDB($base);
+    return $newUser;
 }
 
-$pdo = myOpenDatabase('Warsztaty_2');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//    print_r($_POST);
+    $pdo = myOpenDatabase('Warsztaty_2');
 
-addRandomUser($pdo);
+// addRandomUser($pdo);
+    if (isset($_POST['add_user'])) {
+        echo 'Wybrano dodanie użytkownika<br>';
+        $user = createRandomUser($pdo);
+        $user->saveToDB($pdo);
+        echo $user->getId() . ' ' .
+            $user->getUsername() . '<br>';
+    } elseif (isset($_POST['nr_index'])) {
+        echo 'Wybrano wczytanie użytkownika nr ' . $_POST['nr_index'] . "<br>";
+        $user = User::loadUserById($pdo, $_POST['nr_index']);
+        if ($user) {
+            echo $user->getUsername() . '<br>';
+            echo $user->getEmail() . '<br>';
+        } else {
+            echo 'Takiego użytkownika nie ma<br>';
+        }
+    } elseif (isset($_POST['add_all'])) {
+        echo 'Wybrano wczytanie wszystkich użytkowników' . "<br>";
+        $arrUsers = User::loadAll($pdo);
+        if (empty($arrUsers)) {
+            echo 'Nie udało się wczytać żadnych użytkowników<br>';
+        } else {
+            foreach ($arrUsers as $user) {
+                echo $user->getId() . ' ' .
+                    $user->getUsername() . '<br>';
+            }
+        }
+    } elseif (isset($_POST['modify_index'])) {
+        echo 'Wybrano modyfikację użytkownika' . $_POST['modify_index'] . "<br>";
+        $user = User::loadUserById($pdo, $_POST['modify_index']);
+        if ($user !== false) {
+            echo 'Poprzednio: ' . $user->getId() . ' ' .
+                $user->getUsername() . '<br>';
+            $user1 = createRandomUser($pdo);
+//            $user = clone $user1;
+            // better than below, there is no simple setPassword:
+        $user->setUsername($user1->getUsername());
+        $user->setEmail($user1->getEmail());
+            $user1 = null;
+            $user->saveToDB($pdo);
+
+            echo 'Obecnie: ' . $user->getId() . ' ' .
+                $user->getUsername() . '<br>';
+
+        } else {
+            echo 'Nie zmodyfikowano użytkownika id ' . $_POST['modify_index'];
+        }
+    } elseif (isset($_POST['delete_index'])) {
+        echo 'Wybrano usunięcie użytkownika ' . $_POST['delete_index'] . "<br>";
+        $user = User::loadUserById($pdo, $_POST['delete_index']);
+        $result = false;
+        if ($user !== false) {
+            echo 'Identyfikator w obiekcie: ' . $user->getId() . '<br>';
+            echo 'Pożegnamy się z: ' . $user->getUsername() . '<br>';
+            $result = $user->delete($pdo);
+            if ($result === true) {
+                echo 'Usunięto wskazany element.<br>';
+            }
+        }
+        if ($result === false) {
+            echo "Nie udało się usunąć wskazanego elementu.<br>";
+        }
+    } else {
+        echo 'Błąd w programie<br>';
+    }
+
+}
+
+
+
+?>
+
--- scr/User.php
+++ scr/User.php
@@ -50,7 +50,7 @@ class User
     }
 
     public function saveToDB(PDO $conn) {
-        if ($this->id == -1) {
+        if ($this->id == -1) { // sprawdzanie czy obiektu nie ma już w bazie
             $stmt = $conn->prepare(
               'INSERT INTO Users (username, email, hash_pass)
               VALUES (:username, :email, :pass)'
@@ -62,6 +62,71 @@ class User
                 $this->id = $conn->lastInsertId();
                 return true;
             }
+        } else { // zapisanie obiektu już w bazie, ale zmodyfikowanego
+            $stmt = $conn->prepare(
+                'UPDATE Users SET email=:email, username=:username, 
+              hash_pass=:hash_pass WHERE id=:id');
+            $result = $stmt->execute ([
+                ':id' => $this->is,
+                ':email' => $this->email,
+                ':username' => $this->username,
+                ':hash_pass' => $this->hashPass
+            ]);
+            return $result;
+        }
+        return false;
+    }
+
+    static public function loadUserById (PDO $conn, $id)
+    {
+        $stmt = $conn->prepare('SELECT * FROM Users WHERE id=:id');
+        $result = $stmt->execute([':id' => $id]);
+        if ($result === true && $stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $loadedUser = new User();
+            $loadedUser->id = $row['id'];
+            $loadedUser->username = $row['username'];
+            $loadedUser->hashPass = $row['hash_pass'];
+            $loadedUser->email = $row['email'];
+            return $loadedUser;
+        }
+        return false;
+    }
+    static public function loadAll (PDO $conn)
+    {
+        $stmt = $conn->prepare('SELECT * FROM Users');
+        $result = $stmt->execute();
+        $arr = [];
+        if ($result === true && $stmt->rowCount() > 0) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $loadedUser = new User();
+                $loadedUser->id = $row['id'];
+
+                //długo szukałem błędu, napisałem i źle działało:
+//                $loadedUser->id = $row[$id];
+
+
+                $loadedUser->username = $row['username'];
+                $loadedUser->hashPass = $row['hash_pass'];
+                $loadedUser->email = $row['email'];
+                $arr[] = $loadedUser;
+            }
+        }
+        return $arr;
+    }
+    public function delete (PDO $conn) {
+        $id = $this->getId();
+        echo "Indeks: $id";
+        if ($id != -1) {
+            $stmt = $conn->prepare("DELETE FROM Users"
+             . " WHERE id=:id");
+            $result = $stmt->execute([':id' => $id]);
+            if ($result === true) {
+                echo "Indeks: $id";
+                $this->id = -1;
+                echo "udało się<br>";
+            }
+            return $result;
         }
         return false;
     }
